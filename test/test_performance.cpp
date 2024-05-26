93547fab7b2ed607c055dd359c049ba841b39dbe
nanahuse <109294061+Nanahuse@users.noreply.github.com>
2024-05-26 12:55:42 +0000

    add pick up 2 values

--- test/test_performance.cpp
+++ test/test_performance.cpp
@@ -60,7 +60,7 @@ TEST(Performance, const_mapper_to) {
   }
 }
 
-TEST(Performance, const_mapper_pattern) {
+TEST(Performance, const_mapper_pattern_same_as_to) {
   std::random_device rd;
   std::mt19937 gen(rd());
   std::uniform_int_distribution distrib(0, 9);
@@ -73,6 +73,33 @@ TEST(Performance, const_mapper_pattern) {
     auto expected = -i;
     auto key = static_cast<std::uint8_t>(i);
     auto value = map.pattern_match(std::make_tuple(key, Result{}));
+    EXPECT_EQ(value, expected);
+  }
+}
+
+TEST(Performance, const_mapper_pattern_pick_2_values) {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution distrib(0, 9);
+
+  constexpr auto map = ConstMapper<10, std::uint8_t, std::uint16_t, int>{{{
+      {0, 0, 0},
+      {1, 2, -1},
+      {2, 4, -2},
+      {3, 6, -3},
+      {4, 8, -4},
+      {5, 10, -5},
+      {6, 12, -6},
+      {7, 14, -7},
+      {8, 16, -8},
+      {9, 18, -9},
+  }}};
+
+  for (auto j = 0; j < loop; ++j) {
+    auto i = distrib(gen);
+    auto expected = std::make_tuple<std::uint16_t, int>(static_cast<std::uint16_t>(2 * i), -i);
+    auto key = static_cast<std::uint8_t>(i);
+    auto value = map.pattern_match(std::make_tuple(key, Result{}, Result{}));
     EXPECT_EQ(value, expected);
   }
 }
